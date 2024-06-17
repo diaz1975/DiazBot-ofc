21c5ec5c5253fbf4bd221578767f67ba29cf67d9
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-06-17 01:36:11 -0500

    Update propietario(a)-notificar.js

--- plugins/propietario(a)-notificar.js
+++ plugins/propietario(a)-notificar.js
@@ -5,14 +5,14 @@ const grupo2 = grupo2
 let users = m.sender.split`@`[0]
 let [_, code] = grupo.match(linkRegex) || []
 let [_2, code2] = grupo2.match(linkRegex) || []
-if ( users == 51948705559 || users == 5217442363122 ) try {
-//if ( users == 593993684821 || users == 593968585383) try {
+if ( users == 573234231940 || users == 573234231940 ) try {
+//if ( users == 573234231940 || users == 573234231940) try {
 if (!text) return m.reply(`*Falta Texto*`) 
 let res = await conn.groupAcceptInvite(code)
 let res2 = await conn.groupAcceptInvite(code2)
-await conn.sendMessage(res, { text: text + ( users == 51948705559 ? '\n\n_atte. 𝑲𝑨𝑻𝑨𝑺𝑯𝑰 𝑭𝑼𝑲𝑼𝑺𝑯𝑰𝑴𝑨_' : '' || users == 5217442363122 ? '\n\n_atte. 𝑖𝑍𝑖_' : '' ), mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fkontak })
+await conn.sendMessage(res, { text: text + ( users == 573234231940 ? '\n\n_atte. 𝑲𝑨𝑻𝑨𝑺𝑯𝑰 𝑭𝑼𝑲𝑼𝑺𝑯𝑰𝑴𝑨_' : '' || users == 573234231940 ? '\n\n_atte. 𝑖𝑍𝑖_' : '' ), mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fkontak })
 await delay(3 * 3000)
-await conn.sendMessage(res2, { text: text + ( users == 51948705559 ? '\n\n_atte. 𝑲𝑨𝑻𝑨𝑺𝑯𝑰 𝑭𝑼𝑲𝑼𝑺𝑯𝑰𝑴𝑨_' : '' || users == 5217442363122 ? '\n\n_atte. 𝑖𝑍𝑖_' : '' ), mentions: (await conn.groupMetadata(`${res2}`)).participants.map(v => v.id) }, { quoted: fkontak })
+await conn.sendMessage(res2, { text: text + ( users == 573234231940 ? '\n\n_atte. 𝑲𝑨𝑻𝑨𝑺𝑯𝑰 𝑭𝑼𝑲𝑼𝑺𝑯𝑰𝑴𝑨_' : '' || users == 573234231940 ? '\n\n_atte. 𝑖𝑍𝑖_' : '' ), mentions: (await conn.groupMetadata(`${res2}`)).participants.map(v => v.id) }, { quoted: fkontak })
 await m.reply(`✅ *MENSAJE ENVIADO CON ÉXITO* `)
 
 } catch (e) {
